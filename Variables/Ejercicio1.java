6559d01e06e11f6e2a6ea2069cacf403ddffa765
Santos López <santos10502@gmail.com>
2020-10-20 10:18:32 +0200

    ejercicios Variables

---
+++ Variables/Ejercicio1.java
@@ -0,0 +1,17 @@
+/**
+ *Por Santos
+ */
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    int x = 144;
+    int y;
+    y = 999;
+    
+    System.out.println("x =: " + x);
+    System.out.println("y =  " + y);
+    System.out.println("x + y = " + (x + y));
+    System.out.println("x - y = " + (x - y));
+    System.out.println("x * y = " + (x * y));
+    System.out.println("x / y = " + ((float)x / (float)y));
+  }
+}
---
+++ Variables/Ejercicio2.java
@@ -0,0 +1,12 @@
+/**
+ *Por Santos
+ */
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    String nombre;
+    nombre = "Santos López Lozano";
+    // También se puede: String nombre = Santos López.
+    
+    System.out.println(nombre);
+  }
+}

c8e7ec086066c06d852edfae4c8a2413949617fc
Santos López <santos10502@gmail.com>
2020-10-20 18:41:09 +0200

    ejercicios Variables

---
+++ Variables/Ejercicio7.java
@@ -0,0 +1,12 @@
+/**
+ * Por Santos López
+ */
+ public class Ejercicio7 {
+  public static void main(String[] args) {
+    char unaLetra = 'A';
+    char otraLetra = 'B';
+    String palabra = "lapalabra";
+    
+    System.out.println(unaLetra + " " + otraLetra + " " + palabra);
+  }
+ }
---
+++ Variables/Ejercicio8.java
@@ -0,0 +1,18 @@
+/**
+ * Por Santos López
+ */
+public class Ejercicio8 {
+  public static void main(String[] args) {
+    char l1 = 'N';
+    char l2 = 'u';
+    char l3 = 'n';
+    char l4 = 'c';
+    char l5 = 'a';
+    
+    
+    // String palabra = l1 + l2 + l3 + l4 + l5;
+    String palabra = "" + l1 + l2 + l3 + l4 + l5;
+    
+    System.out.println(palabra);
+  }
+}

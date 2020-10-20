91ad790a39bf09506b3557d8d949ae0e1f334019
Santos López <santos10502@gmail.com>
2020-10-20 18:16:41 +0200

    ejercicios Variables

---
+++ Variables/Ejercicio6.java
@@ -0,0 +1,12 @@
+/**
+ * Por Santos López
+ */
+ public class Ejercicio6 {
+  public static void main(String[] args) {
+    double precioBase = 6.0;
+    double iva = 21 * precioBase / 100;
+    double precioTotal = precioBase + iva;
+
+    System.out.print("el precio total del producto es " + precioTotal);
+  }
+}

db73326678ab90596f46e16951ff57b3ab5e5cf5
Santos López <santos10502@gmail.com>
2020-11-09 10:08:36 +0100

    ultima actualización

---
+++ Bucles/Ejercicio2.java
@@ -0,0 +1,15 @@
+/**
+ * Santos López Lozano
+ */
+public class Ejercicio2 {
+
+  public static void main(String[] args) {
+    
+    int i = 0;
+    
+    while(i <= 100) {
+      System.out.println(i);
+      i+=5;
+    }
+  }
+}
---
+++ Bucles/Ejercicio3.java
@@ -0,0 +1,16 @@
+
+/**
+ * Santos López Lozano
+ */
+public class Ejercicio3 {
+
+  public static void main(String[] args) {
+    
+    int i = 0;
+    
+    do {
+      System.out.println(i);
+      i+=5;
+    } while (i < 101);
+  }
+}

67224506a1983f7ae99526acd135ab6a91bde688
Santos López <santos10502@gmail.com>
2020-11-17 10:02:48 +0100

    ultima actualización

---
+++ Bucles/Ejercicio11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+/**
+ * Santos López Lozano
+ */
+public class Ejercicio11 {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    int numero;
+    
+    System.out.print("Introduzca un número: ");
+    numero = s.nextInt();
+  }
+  
+}

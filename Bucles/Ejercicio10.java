5fdaf43d3bff92bc0b9d84761de528a97f7c7848
Santos López <santos10502@gmail.com>
2020-11-17 09:38:54 +0100

    ultima actualización

--- Bucles/Ejercicio10.java
+++ Bucles/Ejercicio10.java
@@ -1,23 +1,35 @@
 import java.util.Scanner;
 /**
- *Santos López Lozan
+ *Santos López Lozano
  */
 public class Ejercicio10 {
   public static void main(String[] args) {
     
     Scanner s = new Scanner(System.in);
     
-    int numeroDeNotas = 0;
+    int numeroDeNotas = 1;
     long nota;
+    long media;
     
     System.out.println("las notas de las que quiere saber la media.");
     System.out.println("Cuando termines de introducir notas ponga una negativa.");
     
-    do {
+    System.out.print("escriba una nota= ");
+      nota = s.nextInt();
+    long notaFinal = nota;
+    
+    while (nota >= 0) {
       System.out.print("escriba una nota= ");
       nota = s.nextInt();
+      notaFinal = nota + notaFinal;
       numeroDeNotas++;
-    } while (nota >= 0);
-    System.out.println(numeroDeNotas);
+    }
+    numeroDeNotas--;
+    notaFinal = notaFinal - nota;
+    System.out.println("Has introducido= " + numeroDeNotas + " notas");
+    System.out.println("la suma de todas tus notas es= " + notaFinal);
+    media = notaFinal / numeroDeNotas;
+    System.out.println("la nota media es= " + media);
+    
   }
 }

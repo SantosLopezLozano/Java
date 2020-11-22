ce56fec773d4447b9f54e62b4d67729f9f52e2cc
Santos López <santos10502@gmail.com>
2020-11-22 22:14:09 +0100

    ultima actualización

--- Bucles/Ejercicio11.java
+++ Bucles/Ejercicio11.java
@@ -7,9 +7,18 @@ public class Ejercicio11 {
     
     Scanner s = new Scanner(System.in);
     int numero;
+    int cuadrado;
+    int cubo;
     
     System.out.print("Introduzca un número: ");
     numero = s.nextInt();
+    
+    for (int i = 1; i < 6; i++){
+      cuadrado = numero * numero;
+      cubo = numero * numero * numero;
+      numero = numero + 1;
+      System.out.printf("%5d %5d %5d\n", numero, cuadrado, cubo);
+    }
   }
   
 }

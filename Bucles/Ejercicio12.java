3c766187969a3cc480a6e5d51660212fb4db474c
Santos López <santos10502@gmail.com>
2020-11-22 22:14:42 +0100

    ultima actualización

---
+++ Bucles/Ejercicio12.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+/**
+ *Santos López Lozano
+ */
+public class Ejercicio12 {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+  }
+  
+}

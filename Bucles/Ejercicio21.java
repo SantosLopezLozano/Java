3c71cd4cb470f559069f044287c8978dcc46b853
Santos López <santos10502@gmail.com>
2020-11-30 12:19:51 +0100

    ultima actualización

---
+++ Bucles/Ejercicio21.java
@@ -0,0 +1,36 @@
+/**
+ * santos López Lozano
+ */
+public class Ejercicio21 {
+
+  public static void main(String[] args) {
+    
+    System.out.println("ntroduzca números enteros.");
+    System.out.println("Puede terminar mediante la introducción de un número negativo.");
+
+    int numeroIntroducido;
+    int numeroDeElementos = 0;
+    int sumaImpares = 0;
+    int numeroDeElementosImpares = 0;
+    int maximoPar = 0;
+    
+    do {
+      numeroIntroducido = Integer.parseInt(System.console().readLine());
+      
+      if (numeroIntroducido >= 0) {
+        numeroDeElementos++;
+        if ((numeroIntroducido % 2) == 1) {
+          sumaImpares += numeroIntroducido;
+          numeroDeElementosImpares++;
+        } else { 
+          if (numeroIntroducido > maximoPar)
+            maximoPar = numeroIntroducido;
+        }
+      }
+    } while (numeroIntroducido >= 0);
+
+    System.out.println("Ha introducido " + numeroDeElementos + " números");
+    System.out.println("La media de los impares es " + sumaImpares/numeroDeElementosImpares);
+    System.out.println("El máximo de los pares es " + maximoPar);
+  }
+}
---
+++ Números_Aleatorios/Ejercicio2.java
@@ -0,0 +1,48 @@
+/**
+ * Santos López Lozano
+ */
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    
+    String palo = "";
+    String carta = "";
+    
+    int numeroPalo = (int)(Math.random()*4) + 1;
+    
+    switch(numeroPalo) {
+      case 1:
+        palo = "picas";
+        break;
+      case 2:
+        palo = "corazones";
+        break;
+      case 3:
+        palo = "diamantes";
+        break;
+      case 4:
+        palo = "tréboles";
+      default:
+    }
+
+    int numeroCarta = (int)(Math.random()*13) + 1;
+    
+    switch(numeroCarta) {
+      case 1:
+        carta = "As";
+        break;
+      case 11:
+        carta = "J";
+        break;
+      case 12:
+        carta = "Q";
+        break;
+      case 13:
+        carta = "K";
+        break;
+      default:
+        carta = String.valueOf(numeroCarta);
+    }
+    
+    System.out.println(carta + " de " + palo);
+  }
+}

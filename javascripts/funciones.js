514f4a7dcb681ecb07ccd6ef62e873920da1a8fd
Santos López Lozano <santos10502@gmail.com>
2021-01-14 08:17:03 +0100

    Add files via upload

---
+++ javascripts/funciones.js
@@ -0,0 +1,41 @@
+/* funciones javascript*/
+/* funciones javascript para la landing page de Sistemas Informáticos */
+
+/******** js de topnav responsive */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/***** para que el scroll se ejecute de manera suave y progresiva */
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 2500, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+

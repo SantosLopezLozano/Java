7cf2f5d79848f8cba033b21aa365c54456f31963
Santos López <santos10502@gmail.com>
2021-03-24 10:50:12 +0100

    ejercicios 3 y 4

--- Colecciones_y_Diccionarios/ejercicio3/Ejercicio3.java
+++ Colecciones_y_Diccionarios/ejercicio3/Ejercicio3.java
@@ -1,10 +1,27 @@
 package ejercicio3;
-/**
- * @author Santos López Lozano
- */
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Ejercicio3 {
   
   public static void main(String args[]) {
     
+    Scanner scan = new Scanner(System.in);
+    
+    ArrayList<Integer> listaDeNumeros = new ArrayList<Integer>();
+    
+    System.out.println("escribe 10 numeros: ");
+    
+    for (int i = 0; i < 10; i++) {
+      listaDeNumeros.add(scan.nextInt());
+    }
+    
+    System.out.println("sin ordenar " + listaDeNumeros);
+    
+    Collections.sort(listaDeNumeros);
+    
+    System.out.println("ordenada " + listaDeNumeros);
   }
 }
---
+++ Colecciones_y_Diccionarios/ejercicio4/Ejercicio4.java
@@ -0,0 +1,29 @@
+package ejercicio4;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Ejercicio4 {
+  
+  public static void main(String args[]) {
+    
+    Scanner scan = new Scanner(System.in);
+    
+    ArrayList<String> listaDeNombres= new ArrayList<String>();
+    
+    System.out.println("Escribe 10 palabras: ");
+    
+    for (int i = 0; i < 10; i++) {
+      listaDeNombres.add(scan.next());
+    }
+    
+    System.out.println("original " + listaDeNombres);
+      
+    Collections.sort(listaDeNombres);
+      
+    System.out.println("ordenada" + listaDeNombres);
+  }
+}
+
+  
